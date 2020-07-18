11c4f6343506e9876d11c180a76c8fe33f813e0a
Ankit Hans <ankithans1947@gmail.com>
2020-07-18 13:46:26 +0530

    added login routes and models

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const app = express();
 // Connect Database
 connectDB();
 
-app.get('/', (req, res) =>
-  res.json({
-    msg: 'Welcome to the API...',
-  })
-);
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
