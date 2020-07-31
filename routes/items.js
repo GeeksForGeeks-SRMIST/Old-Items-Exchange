ee9062028351c5df0aeff63ed8a8760a18b36871
Ankit Hans <ankithans1947@gmail.com>
2020-07-31 21:35:19 +0530

    added update and delet items routes

--- routes/items.js
+++ routes/items.js
@@ -4,6 +4,10 @@ const Item = require('../models/Item');
 const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
 const auth = require('../middleware/auth');
+const mongoose = require('mongoose');
+const { response } = require('express');
+const { findByIdAndDelete } = require('../models/Item');
+const ObjectId = mongoose.Types.ObjectId;
 
 // @route       POST api/item
 // @dsc         Create new item
@@ -25,15 +29,7 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    const {
-      _id,
-      item_name,
-      price,
-      number,
-      address,
-      location,
-      images,
-    } = req.body;
+    const { item_name, price, number, address, location, images } = req.body;
 
     try {
       let item = await Item.findOne({ item_name });
@@ -54,7 +50,7 @@ router.post(
       });
 
       await item.save();
-      return res.json({ item });
+      return res.json({ msg: 'Item Added Successfully!' });
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');
@@ -62,44 +58,87 @@ router.post(
   }
 );
 
-// // @route       PUT api/item/:id
-// // @dsc         Update item
-// // @access      Private
-// router.put('/:id', auth, async (req, res) => {
-//   const { item_name, price, number, address, location, images } = req.body;
+// @route       PUT api/item/:id
+// @dsc         Update item
+// @access      Private
+router.put(
+  '/:id',
+  [
+    check('item_name', 'Please add a item name').notEmpty(),
+    check('price', 'Please include price for item').notEmpty().isNumeric(),
+    check('number', 'Please Enter a valid number').isLength({
+      min: 10,
+    }),
+    check('address', 'Please add a address').notEmpty(),
+    check('location', 'Please add a location').notEmpty(),
+  ],
+  auth,
+  async (req, res) => {
+    const { item_name, price, number, address, location, images } = req.body;
 
-//   // Build a item Object
-//   const itemFields = {};
-//   if (item_name) itemFields.item_name = item_name;
-//   if (price) itemFields.price = price;
-//   if (number) itemFields.number = number;
-//   if (address) itemFields.address = address;
-//   if (location) itemFields.location = location;
-//   if (images) itemFields.images = images;
+    const itemFields = {};
+    if (item_name) itemFields.item_name = item_name;
+    if (price) itemFields.price = price;
+    if (number) itemFields.number = number;
+    if (address) itemFields.address = address;
+    if (location) itemFields.location = location;
+    if (images) itemFields.images = images;
 
-//   try {
-//     // console.log(req.params.id);
-//     let item = await Item.findById(req.params.id);
+    let item = new Item({
+      item_name,
+      price,
+      number,
+      address,
+      location,
+      images,
+      user: req.user.id,
+    });
+    try {
+      item = await Item.findById(req.params.id);
+      if (!item) return res.status(404).json({ msg: 'Item not found' });
 
-//     if (!item) return res.status(404).json({ msg: 'item not found' });
+      // Make sure user owns Contact
+      if (item.user.toString() !== req.user.id) {
+        return res.status(401).json({ msg: 'Not authorized' });
+      }
+      item = await Item.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+      });
+      return res.json({ msg: 'Item Updated Successfully' });
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
-//     // Make sure user owns item
-//     if (item.user.toString() !== req.user.id) {
-//       return res.status(401).json({ msg: 'Not authorized' });
-//     }
+// @route       PUT api/item/:id
+// @dsc         Update item
+// @access      Private
+router.get('/list', auth, async (req, res) => {
+  try {
+    let items = await Item.find({});
+    return res.json({ items });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
-//     item = await Item.findByIdAndUpdate(req.params);
-//   } catch (err) {
-//     console.error(err.message);
-//     res.status(500).send('Server Error');
-//   }
-// });
+// @route       DELETE api/contacts/:id
+// @dsc         Delete contact
+// @access      Private
+router.delete('/:id', async (req, res) => {
+  try {
+    let item = await Item.findById(req.params.id);
+    if (!item) return res.status(404).json({ msg: 'Item not found' });
 
-// // @route       DELETE api/contacts/:id
-// // @dsc         Delete contact
-// // @access      Private
-// router.delete('/:id', (req, res) => {
-//   res.send('Delete contact');
-// });
+    await Item.findByIdAndDelete(req.params.id);
+    return res.json({ msg: 'Item deleted successfully' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
